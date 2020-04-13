36e793c19bb52750901b8c13c0eba1bd6642fa4f
EC2 Default User <ec2-user@ip-172-31-43-243.us-east-2.compute.internal>
2020-04-13 23:54:05 +0000

    wip

--- server/server.py
+++ server/server.py
@@ -4,8 +4,12 @@ import sys
 from pydub import AudioSegment
 import json
 import struct
+import numpy as np
 
 aud = AudioSegment.empty()
+STATE = {'audio': AudioSegment.empty()}
+
+count = 1
 
 if len(sys.argv) < 2:
     port = 3000
@@ -14,25 +18,41 @@ else:
 
 async def hello(websocket, path):
     while True:
+        
         data = await websocket.recv()
-        asyncio.create_task(handle_message(websocket, data))
+        data_loaded = json.loads(data)
+    # print(data_loaded)
+    
+        if data_loaded["type"] == "data":
+            byte_string = b''
+            for k,v in data_loaded["data"].items():
+                byte_string += struct.pack('f',v)
+            STATE['audio'] = STATE['audio'].append(AudioSegment(data=byte_string,sample_width=4,frame_rate=48000,channels=1), crossfade=0)
+    
+        elif data_loaded["type"] == "stop_metronome":
+            print("here")
+            STATE['audio'].export("anything.mp3", format="mp3")
 
-async def handle_message(websocket, data):
+        await websocket.send(data)
+        #print(aud)
+        #asyncio.create_task(handle_message(websocket, data, aud))
+
+async def handle_message(websocket, data, segment):
     data_loaded = json.loads(data)
     # print(data_loaded)
-
+    
     if data_loaded["type"] == "data":
         byte_string = b''
         for k,v in data_loaded["data"].items():
             byte_string += struct.pack('f',v)
-        
-        aud.append(AudioSegment(data=byte_string,sample_width=1024,frame_rate=48000,channels=1), crossfade=0)
+        segment = segment.append(AudioSegment(data=byte_string,sample_width=4,frame_rate=48000,channels=1), crossfade=0)
     
     elif data_loaded["type"] == "stop_metronome":
-        aud.export("anything.mp3", format="mp3")
+        print("here")
+        segment.export("anything.mp3", format="mp3")
 
     await websocket.send(data)
 
-start_server = websockets.serve(hello, "localhost", port)
+start_server = websockets.serve(hello, "0.0.0.0", port)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
