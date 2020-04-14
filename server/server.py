c35da7a3aea503d094293e6b9c5c4783d74a412a
EC2 Default User <ec2-user@ip-172-31-43-243.us-east-2.compute.internal>
2020-04-14 15:52:53 +0000

    wip

--- server/server.py
+++ server/server.py
@@ -22,36 +22,27 @@ async def hello(websocket, path):
         data = await websocket.recv()
         data_loaded = json.loads(data)
     # print(data_loaded)
-    
+
         if data_loaded["type"] == "data":
+            for i in range(len(data_loaded["data"])):
+                data_loaded["data"][i] = [struct.unpack('f', struct.pack('f', v)) for k, v in data_loaded["data"][i].items()]
+            await websocket.send(json.dumps(data_loaded))
+            print("next")
+            continue 
             byte_string = b''
-            for k,v in data_loaded["data"].items():
-                byte_string += struct.pack('f',v)
-            STATE['audio'] = STATE['audio'].append(AudioSegment(data=byte_string,sample_width=4,frame_rate=48000,channels=1), crossfade=0)
+            for i in range(len(data_loaded["data"][0])):
+                byte_string += struct.pack('f',data_loaded["data"][0][i][0])
+                byte_string += struct.pack('f',data_loaded["data"][1][i][0])
+
+            STATE['audio'] = STATE['audio'].append(AudioSegment(data=byte_string,sample_width=4,frame_rate=48000,channels=2), crossfade=0)
     
         elif data_loaded["type"] == "stop_metronome":
             print("here")
-            STATE['audio'].export("anything.mp3", format="mp3")
+            STATE['audio'].export("/home/ec2-user/anything.mp3", format="mp3")
+            STATE['audio'] = AudioSegment.empty()
 
-        await websocket.send(data)
-        #print(aud)
-        #asyncio.create_task(handle_message(websocket, data, aud))
+        await websocket.send(json.dumps(data_loaded))
 
-async def handle_message(websocket, data, segment):
-    data_loaded = json.loads(data)
-    # print(data_loaded)
-    
-    if data_loaded["type"] == "data":
-        byte_string = b''
-        for k,v in data_loaded["data"].items():
-            byte_string += struct.pack('f',v)
-        segment = segment.append(AudioSegment(data=byte_string,sample_width=4,frame_rate=48000,channels=1), crossfade=0)
-    
-    elif data_loaded["type"] == "stop_metronome":
-        print("here")
-        segment.export("anything.mp3", format="mp3")
-
-    await websocket.send(data)
 
 start_server = websockets.serve(hello, "0.0.0.0", port)
 asyncio.get_event_loop().run_until_complete(start_server)
