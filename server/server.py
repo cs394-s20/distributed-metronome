c648921b95863057270b8fed744c79a649b43700
EC2 Default User <ec2-user@ip-172-31-43-243.us-east-2.compute.internal>
2020-04-16 01:51:26 +0000

    wip

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ import json
 import struct
 import audioop
 import datetime
+import time
 
 if len(sys.argv) < 2:
     port = 3000
@@ -22,18 +23,23 @@ async def hello(websocket, path):
     try:
         while True:
             data = await websocket.recv()
+            t = time.time()
             data_loaded = json.loads(data)
             if data_loaded["type"] == "data":
                 for channel in range(2):
-                    data_loaded["data"][channel] = [x for x in data_loaded["data"][channel]]
+                    data_loaded["data"]["channels"][channel] = [x for x in data_loaded["data"]["channels"][channel]]
                                 
+                #await websocket.send(json.dumps(data_loaded))
                 asyncio.create_task(send_message(websocket, json.dumps(data_loaded)))
 
             elif data_loaded["type"] == "start_metronome":
                 start_time = datetime.datetime.utcnow().timestamp() + 5
                 data_loaded = {"type": "start_metronome", "data":{"ts":  start_time}}
                 asyncio.create_task(send_all(json.dumps(data_loaded)))
+            elif data_loaded["type"] == "stop_metronome":
+                asyncio.create_task(send_all(json.dumps(data_loaded)))
 
+            print(time.time() - t)
     except Exception as e:
         print(e)
     STATE['connections'].remove(websocket)
