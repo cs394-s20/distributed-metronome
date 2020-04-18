a95cb41ccfd9dc39fd5475cbc19272ae00ba1a7a
unknown <rochellecompendio2021@u.northwestern.edu>
2020-04-18 18:57:10 -0500

    added a prompt for you to change the name of the file

--- src/components/03_Organisms/StartRecording.js
+++ src/components/03_Organisms/StartRecording.js
@@ -1,38 +1,39 @@
 import React, { useState } from 'react';
 import { ReactMic } from '@cleandersonlobo/react-mic';
 import '../../styles/styles.scss';
-// import RoomClient from '../../shared/RoomClient';
 
-// const roomClient = new RoomClient('ws://18.217.104.101:3000');
 
-var recording = false;
+var recording = false; // for sending to backend
 
 function StartRecording(props) {
-  const [record, setRecord] = useState(false);
+  const [record, setRecord] = useState(false); // record = if it's recording or not
 
-
+  // function called when you press "start recording" or "stop recording"
   const toggleRecording = () => {
-    if (!record){
+    if (!record) {
       document.getElementById("button-1").disabled = true;
       var timeleft = 3;
-      var downloadTimer = setInterval(function(){
-        if(timeleft <= 0){
+      var downloadTimer = setInterval(function () {
+
+        // when timer is up
+        if (timeleft <= 0) {
           document.getElementById("button-1").disabled = false;
           recording = true;
           clearInterval(downloadTimer);
           setRecord(!record)
           document.getElementById("countdown").innerHTML = "";
-        } else {
+        }
+        // when timer is still going
+        else {
           document.getElementById("countdown").innerHTML = timeleft;
         }
         timeleft -= 1;
       }, 1000);
     }
-    else{
+    else {
       recording = false;
       setRecord(!record)
     }
-
   }
 
 
@@ -63,13 +64,15 @@ function StartRecording(props) {
     // console.log('chunk of real-time data is: ', recordedBlob);
   }
 
+  // function that's called when you stop recording
   const onStop = (recordedBlob) => {
     // console.log('recordedBlob is: ', recordedBlob);
     // console.log("Blah: ", recordedBlob["blobURL"]);
 
+    const name = prompt('Please enter a name for the recording!'); // prompts you to name the file
     const link = document.createElement('a');
     link.href = recordedBlob["blobURL"];
-    link.download = "recording"
+    link.download = name;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -78,23 +81,23 @@ function StartRecording(props) {
 
 
   var buttonMessage = record ? "Stop Recording" : "Start Recording"
-      return (
-      <div>
-        <div className="flexRow justifyContentCenter">
-          <ReactMic
-            record={record}
-            className="sound-wave"
-            onStop={onStop}
-            onData={onData}
-            strokeColor="white"
-            backgroundColor="black"
-            mimeType="audio/mp3" />
-        </div>
-        <div className="flexRow justifyContentCenter">
-          <button onClick={toggleRecording} type="button" id="button-1" className={record ? "button--red" : "button--green"}>{buttonMessage}</button>
-        </div>
-        <div id="countdown"></div>
+  return (
+    <div>
+      <div className="flexRow justifyContentCenter">
+        <ReactMic
+          record={record}
+          className="sound-wave"
+          onStop={onStop}
+          onData={onData}
+          strokeColor="white"
+          backgroundColor="black"
+          mimeType="audio/mp3" />
       </div>
+      <div className="flexRow justifyContentCenter">
+        <button onClick={toggleRecording} type="button" id="button-1" className={record ? "button--red" : "button--green"}>{buttonMessage}</button>
+      </div>
+      <div id="countdown"></div>
+    </div>
   )
 }
 export default StartRecording;
