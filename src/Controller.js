38a4e5901fa6c52fc58299cd3d9c5187d07fe7a6
unknown <rochellecompendio2021@u.northwestern.edu>
2020-04-21 18:10:35 -0500

    changed room code and user count format, moved light/dark mode to footer

--- src/Controller.js
+++ src/Controller.js
@@ -1,70 +1,51 @@
-// import React, { useState } from 'react';
-// import Home from './components/03_Organisms/Home';
-// import JoinSession from './components/03_Organisms/JoinSession';
-// import StartRecording from './components/03_Organisms/StartRecording';
-// import Confirmation from './components/03_Organisms/Confirmation';
-
-// function Controller(props){
-//     const [page, setPage] = useState('home')
-    
-//     if (page === 'home'){
-//         return <Home setPage={setPage} appClient={props.appClient}/>
-//     }
-
-//     if (page === 'joinSession'){
-//         return <JoinSession setPage={setPage} appClient={props.appClient}/>
-//     }
-
-//     if (page === 'startRecording'){
-//         return <StartRecording setPage={setPage} appClient={props.appClient}/>
-//     }
-
-//     if (page === 'confirmation'){
-//         return <Confirmation setPage={setPage} appClient={props.appClient}/>
-//     }
-// }
-
-// export default Controller;
-
 import React, { useState } from 'react';
-import Header from './components/02_Molecules/Header';
+import Header from './components/02_Molecules/Header/Header';
+import Footer from './components/02_Molecules/Footer/Footer';
 import Home from './components/03_Organisms/Home';
 import JoinSession from './components/03_Organisms/JoinSession';
 import StartRecording from './components/03_Organisms/StartRecording';
 import Confirmation from './components/03_Organisms/Confirmation';
 
 function Controller(props){
-    const [page, setPage] = useState('home')
+    const [page, setPage] = useState('home');
+    const [mode, setMode] = useState('dark');
 
     let currPage;
     let leaveButton;
+    let roomCode;
 
     if (page === 'home'){
         leaveButton = false;
-        currPage = <Home setPage={setPage} appClient={props.appClient}/>
+        roomCode = false;
+        currPage = <Home setPage={setPage} appClient={props.appClient} mode={mode}/>
     }
 
     if (page === 'joinSession'){
         leaveButton = false;
-        currPage = <JoinSession setPage={setPage} appClient={props.appClient}/>
+        roomCode = false;
+        currPage = <JoinSession setPage={setPage} appClient={props.appClient} mode={mode}/>
     }
 
     if (page === 'startRecording'){
         leaveButton = true;
+        roomCode = true;
         currPage = <StartRecording setPage={setPage} appClient={props.appClient}/>
     }
 
     if (page === 'confirmation'){
         leaveButton = true;
-        currPage = <Confirmation setPage={setPage} appClient={props.appClient}/>
+        roomCode = true;
+        currPage = <Confirmation setPage={setPage} appClient={props.appClient} mode={mode}/>
     }
 
     return(
         <React.Fragment>
-            <Header setPage={setPage} leaveButton={leaveButton} appClient={props.appClient}/>
+            <Header setPage={setPage} appClient={props.appClient} leaveButton={leaveButton} roomCode={roomCode} mode={mode}/>
             <div className="content" style = { { backgroundImage : "url(" + require("./bg.png") + ")" } }>
                 {currPage}
+                <Footer setMode={setMode}/>
             </div>
+            
         </React.Fragment>
     )
 }
---
+++ src/components/02_Molecules/Header/Header.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import '../../../styles/styles.scss';
+
+function Header(props) {
+    let leaveButton;
+    let roomCode;
+    let roomCount;
+
+    if (props.leaveButton){
+         leaveButton = (<button className="button--orange" onClick={() => props.setPage('home')}>Leave Session</button>);
+    }
+
+    if (props.roomCode){
+        roomCode = (<span className="roomCode">Room Code: {props.appClient.roomClient.roomCode}</span>);
+    }
+
+    if (props.appClient.roomClient.roomCode){
+        var roomClient = props.appClient.roomClient;
+        function UserCount(){
+            let [count, setCount] = useState(1);
+            roomClient.onListUsers = function(data){
+                setCount(data.count)
+            }.bind(setCount);
+            return (
+                <div id="user_count">
+                    Users: {count}
+                </div>
+            )
+        }
+        UserCount = UserCount.bind(roomClient);
+        roomCount = UserCount();
+    }
+
+    return (
+        <div className="header">
+            <h1>Metronome</h1>
+            <div className="flexRow">
+                <div className={(props.mode == "dark") ? "header__roomInfo textDarkMode" : "header__roomInfo textWhiteMode"}>
+                    {roomCode}
+                    {roomCount}
+                </div>
+                {leaveButton}
+            </div>
+        </div>
+    )
+}
+
+export default Header;
