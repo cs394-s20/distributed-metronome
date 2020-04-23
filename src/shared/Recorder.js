5dd76ef5f59338a92cb8c747df3a59d200ae77dc
petersenea <Petersenea12@gmail.com>
2020-04-23 12:54:31 -0500

    Added a delayed download button

--- src/shared/Recorder.js
+++ src/shared/Recorder.js
@@ -2,7 +2,7 @@ import audioBufferToWav from 'audiobuffer-to-wav';
 
 Array.prototype.extend = function (other_array) {
     /* You should include a test to check whether other_array really is an array */
-    other_array.forEach(function(v) {this.push(v)}, this);
+    other_array.forEach(function (v) { this.push(v) }, this);
 }
 
 export default class Recorder {
@@ -18,7 +18,7 @@ export default class Recorder {
         this.chunks = [[], []];
         this.chunks_recorded = 0;
         this.chunks_returned = 0;
-        
+
 
         this.record = false;
         this.processor = processor
@@ -30,19 +30,19 @@ export default class Recorder {
         this.context = new AudioContext();
         const source = this.context.createMediaStreamSource(stream);
         const processor = this.context.createScriptProcessor(8192, 2, 1);
-        
+
         source.connect(processor);
         processor.connect(this.context.destination);
 
         processor.onaudioprocess = function (e) {
             // Do something with the data, e.g. convert it to WAV
-            
+
             if (this.record) {
                 this.processor(e);
             }
         }.bind(this);
 
-        
+
     };
 
     startRecording = function () {
@@ -56,67 +56,70 @@ export default class Recorder {
         this.record = false;
     }
 
-    playBuffer(buffer){
+    playBuffer(buffer) {
         var myArrayBuffer = this.context.createBuffer(2, 8192, 48000);
-        
+
         for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-            
+
             myArrayBuffer.copyToChannel(Float32Array.from(buffer[channel]), channel);
-            
+
         }
-        
-        var test = this.context.createBufferSource();
 
-        // set the buffer in the AudioBufferSourceNode
-        test.buffer = myArrayBuffer;
+        // var test = this.context.createBufferSource();
 
-        // connect the AudioBufferSourceNode to the
-        // destination so we can hear the sound
-        test.connect(this.context.destination);
+        // // set the buffer in the AudioBufferSourceNode
+        // test.buffer = myArrayBuffer;
 
-        // start the source playing
-        test.start();
+        // // connect the AudioBufferSourceNode to the
+        // // destination so we can hear the sound
+        // test.connect(this.context.destination);
+
+        // // start the source playing
+        // test.start();
 
         this.saveChunk(buffer);
-        
+
 
     }
 
-    saveChunk(buffer){
+    saveChunk(buffer) {
         this.chunks_returned += 1;
-        for (var c = 0; c < 2; c++){
+        for (var c = 0; c < 2; c++) {
             this.chunks[c].extend(buffer[c]);
         }
     }
 
     // async saveRecording(){
-    saveRecording(){
+    saveRecording() {
+
+        if (this.chunks[0].length != 0) {
 
 
-        var myArrayBuffer = this.context.createBuffer(2, this.chunks[0].length, 48000);
-        
-        for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-            
-            myArrayBuffer.copyToChannel(Float32Array.from(this.chunks[channel]), channel);
-            
+            var myArrayBuffer = this.context.createBuffer(2, this.chunks[0].length, 48000);
+
+            for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
+
+                myArrayBuffer.copyToChannel(Float32Array.from(this.chunks[channel]), channel);
+
+            }
+            console.log(myArrayBuffer);
+            console.log(audioBufferToWav(myArrayBuffer));
+
+
+            var element = document.createElement('a');
+            element.href = URL.createObjectURL(new Blob([new DataView(audioBufferToWav(myArrayBuffer))], {
+                type: 'audio/wav'
+            }));
+
+            const name = prompt('Please enter a name for the recording!'); // prompts you to name the file
+            element.download = name;
+
+            // element.download = "recording.wav";
+            element.style.display = 'none';
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
         }
-        console.log(myArrayBuffer);
-        console.log(audioBufferToWav(myArrayBuffer));
-
-
-        var element = document.createElement('a');
-        element.href = URL.createObjectURL(new Blob([new DataView(audioBufferToWav(myArrayBuffer))], {
-            type: 'audio/wav'
-        }));
-
-        const name = prompt('Please enter a name for the recording!'); // prompts you to name the file
-        element.download = name;
-
-        // element.download = "recording.wav";
-        element.style.display = 'none';
-        document.body.appendChild(element);
-        element.click();
-        document.body.removeChild(element);
     }
 
 }
