724bd6f95ece8504f63596303ae56ab3a057fa6a
petersenea <Petersenea12@gmail.com>
2020-04-30 12:42:57 -0500

    i guess this is ok

--- src/shared/AppClient.js
+++ src/shared/AppClient.js
@@ -4,10 +4,15 @@ import Recorder from './Recorder';
 export default class AppClient {
     constructor(){
         this.roomClient = new RoomClient('wss://dm.johnflaboe.com');
+        // this.roomClient = new RoomClient('ws://localhost:5000');
+
         this.recorder = new Recorder();
 
-        this.roomClient.onData = function(e){
+        this.isFinal = false;
+
+        this.roomClient.onData = function(e, isFinal){
             console.log(e.id);
+            console.log(isFinal);
             e = e.channels;
             e[0] = new Float32Array(e[0]);
             e[1] = new Float32Array(e[1]);
@@ -18,7 +23,7 @@ export default class AppClient {
             var channels = [];
             channels.push(Array.prototype.slice.call(e.inputBuffer.getChannelData(0)));
             channels.push(Array.prototype.slice.call(e.inputBuffer.getChannelData(1)));
-            this.roomClient.sendMedia({channels: channels, id: ++this.recorder.chunks_recorded});
+            this.roomClient.sendMedia({channels: channels, id: ++this.recorder.chunks_recorded}, this.isFinal );
         }.bind(this);
     }
 
