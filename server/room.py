0c1702f26481ce4782d654cb736689af397e6eb1
EC2 Default User <ec2-user@ip-172-31-6-234.us-east-2.compute.internal>
2020-05-04 20:27:30 +0000

    changed stream image

--- server/room.py
+++ server/room.py
@@ -6,6 +6,7 @@ import time
 import json
 import struct
 import subprocess
+import asyncio
 
 
 class Room:
@@ -19,6 +20,9 @@ class Room:
         self.stream_started = False
         self.starting_stream = False
         self.last_chunk = -1
+        self.event = asyncio.Event()
+        self.event.set()
+        self.last_chunk_written = 1
 
     def generate_pipes(self):
         self.audio_channel_paths = ['/tmp/' + self.code + str(i) for i in range(2)]
@@ -30,7 +34,7 @@ class Room:
         possible = string.ascii_uppercase + '0123456789'
         return ''.join([random.choice(possible) for i in range(4)])
 
-    def combine_chunks(self, chunk_id):
+    async def combine_chunks(self, chunk_id):
         self.combined_data[chunk_id] = [[0 for j in range(16384)] for i in range(2)]
         for u in self.users:
             try:
@@ -44,9 +48,14 @@ class Room:
         if self.starting_stream:
             self.start_stream()
         if self.stream_started: 
-            for j in range(len(self.combined_data[chunk_id][ch])):
+            while self.last_chunk_written < chunk_id:
+                await self.event.wait()
+            self.event.clear()
+            for j in range(len(self.combined_data[chunk_id][0])):
                 for ch in range(2):
                     self.audio_writer.write(struct.pack('>f', self.combined_data[chunk_id][ch][j]))
+            self.last_chunk_written += 1
+            self.event.set()
                 #self.combined_data[chunk_id][ch][j] = max(-1.0, min(1.0, self.combined_data[chunk_id][ch][j]))
                 
     def start_stream(self):
