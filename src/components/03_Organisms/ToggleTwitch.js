e94c40bb7ca464bc27f6fc609ea0fcf6af5181f5
Bradley Ramos <bradleymramos@gmail.com>
2020-05-06 21:37:04 -0500

    twitch url link

--- src/components/03_Organisms/ToggleTwitch.js
+++ src/components/03_Organisms/ToggleTwitch.js
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 function ToggleTwitch(props) {
     const roomClient = props.appClient.roomClient;
     const [twitch, setTwitch] = useState(false);
-    var btnMessage = twitch ? "Streaming to Twitch" : "Not Streaming to Twitch";
+    const hard_link = "https://www.twitch.tv/fendull"
+    var btnMessage = twitch ? "Streaming to: " : "Not Streaming to Twitch";
+    var twitchLink = twitch ? hard_link : '';
 
     const makeToggleRequest = () => {
         if (twitch) {
@@ -17,7 +19,9 @@ function ToggleTwitch(props) {
     }
 
     return (
-        <button type="button" onClick={makeToggleRequest} className={twitch ?  "button--purple-long": "button--orange-long" } disabled={props.record ? "disabled" : ""}>{btnMessage}</button>
+        <button type="button" onClick={makeToggleRequest} className={twitch ?  "button--purple-long": "button--orange-long" } disabled={twitchLink}>
+            <span>{btnMessage}<a href={twitchLink} style={{display: "table-cell", color:"white"}} target="_blank">{twitchLink}</a></span>
+        </button>
     );
 
 }
